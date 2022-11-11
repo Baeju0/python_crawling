eeeec9e916638c8a9c1e43af3a38a26cc6238d9b
Baeju0 <83051136+Baeju0@users.noreply.github.com>
2022-11-11 15:12:33 +0900

    beautifulSoup_test

--- beautifulSoup_test.py
+++ beautifulSoup_test.py
@@ -22,7 +22,7 @@ html = """
         <p align="center"> 텍스트 내용1 </p>
         <p align="center"> 텍스트 내용2 </p>
         <p align="center"> 텍스트 내용3 </p>
-        <img src = "">
+        <img src = "c:\\Users\\test_img.png" width="250">
     </body>
 </html>
 """
@@ -48,3 +48,63 @@ bs.find('p', align="left")
 # all() 함수 - 모든 결괏값 가져오기
 bs = BeautifulSoup(html,'html.parser')
 bs.find_all('p') # 속성과 관계없이 ''에 해당되는 내용을 모두 가져옴
+
+
+# p태그와 img태그 같이 찾고 싶을 때
+body_tag = bs.find('body') # body태그 부분을 변수에 담기
+
+list_1 = body_tag.find_all(['p','img']) # 태그 여러개 가져오기 → 리스트[] 사용
+for tag in list_1 :
+    print(tag)
+# >> <p align="center"> 텍스트 내용1 </p>
+# <p align="center"> 텍스트 내용2 </p>
+# <p align="center"> 텍스트 내용3 </p>
+# <img src="c:\Users\test_img.png"/>
+
+
+# 특정 문자열만 가져오고 싶어!
+bs.find_all('p') # >> p태그 가져와주세요
+# >> [<p align="center"> 텍스트 내용1 </p>, <p align="center"> 텍스트 내용2 </p>, <p align="center"> 텍스트 내용3 </p>]
+# 글자(내용)만 찾고 싶은데, 태그와 속성 이런 거 다 나옴 ↑
+# 검색해보니 내부 항목만 결과로 얻어올 수 있게 .text 사용
+print(bs.text)
+# >> 텍스트 내용1 텍스트 내용2 텍스트 내용3
+
+# 또한 limit 키워드 사용, 2개의 p태그를 불러옴
+bs.find_all("p",limit=2)
+
+
+# 또한
+# 정규식으로 특정 문자열 가져오기!
+import re # 정규식 모듈
+bs.find_all(text=re.compile("text"))
+
+
+# 찾고 싶은 패턴 지정, p로 시작하는 부분
+tags = bs.find_all(re.compile("^p"))
+tags
+# >> [<p align="center"> 텍스트 내용1 </p>, <p align="center"> 텍스트 내용2 </p>, <p align="center"> 텍스트 내용3 </p>]
+
+
+# 중복되는 태그 구분하기
+bs.find_all(align="center")
+# >> align속성이 "center"인 것만 불러옴
+
+bs.find_all(width="250")
+# >> width속성이 "250"인 것만 불러옴
+
+
+# String 키워드를 이용해 문장 가져오기
+body_tag = bs.find('body') # body 태그 안에 있는 모든 태그 가져오기
+p_tag = body_tag.find('p')
+p_tag.string #string만 가져옴(하나만 가져옴)
+
+strings = body_tag.strings # string 다 보고 싶다! for문 이용
+for string in strings :
+    print(string)
+
+
+# 많이 사용하는 함수 get_text()
+# 특정 태그를 지정하면 그 태그 안에 있는 텍스트 추출
+body_tag = bs.find('body')
+body_tag.get_text('-',strip=True) # '-'텍스트를 구분해주는 기호, #strip은 출력되는 내용에서 \n(띄어쓰기) 기호를 지워줌
